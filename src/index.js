47e6019157f7cc70b8a41a461d99402b57e823cb
SpzGen <Zvezdochkin@mail.ru>
2021-03-11 23:10:38 +0300

    add solutions task
    add solutions task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let temp = "";
+  for (let y = bracketsConfig.length + 1; y > 0; y--) {
+      for (let i = 0; i < bracketsConfig.length; i++) {
+          for (let item of bracketsConfig[i]) {
+              temp += item;
+          }
+          while (str.includes(temp)) {
+              str = str.replace(temp, "");
+          }
+          temp = "";
+      }
+  }
+  return ((str.length === 0) ?  true :  false);
+};
